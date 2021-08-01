74f15cabdfbf3a1c592089aed338942871ebfcce
Matthias Klebe <matthias.klebe@gmail.com>
2021-08-01 12:21:17 +0200

    Adds debug functions

--- src/app/song-list.service.ts
+++ src/app/song-list.service.ts
@@ -1,7 +1,10 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { BoardLineItem } from './previous-lists/lists';
 import { Observable } from 'rxjs';
+
+declare global {
+  interface Window { updatePastIndicies: Function }
+}
 
 @Injectable({
   providedIn: 'root'
@@ -10,7 +13,9 @@ export class SongListService {
   private url: string = 'https://top100-bingo.herokuapp.com/songlist';
   constructor(
     private readonly httpClient: HttpClient
-  ) { }
+  ) {
+    window.updatePastIndicies = this.updatePastIndicies;
+  }
 
   getSongList(): Observable<any> {
     return this.httpClient.get(this.url);
@@ -18,5 +23,15 @@ export class SongListService {
 
   searchSong(category: string, artist: string, song: string): Observable<any> {
     return this.httpClient.get(`${this.url}/${category}/${artist}/${song}`)
+  }
+
+  async updatePastIndicies(): Promise<any> {
+    Promise.all([
+      this.httpClient.get(`${this.url}/Top100Family/updateindex`).toPromise(),
+      this.httpClient.get(`${this.url}/Top100Numbers/updateindex`).toPromise(),
+      this.httpClient.get(`${this.url}/Top100Animals/updateindex`).toPromise(),
+      this.httpClient.get(`${this.url}/Top100Drugs/updateindex`).toPromise(),
+      this.httpClient.get(`${this.url}/Top100Eighties/updateindex`).toPromise(),
+    ]).then(() => console.log('### All indicies updated ###'))
   }
 }
