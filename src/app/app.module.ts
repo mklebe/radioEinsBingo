fbaec043f78025cb18d83400fe9068240d450f58
Matthias Klebe <matthiasklebe@gmail.com>
2022-07-06 22:31:49 +0200

    adds angular material to the project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { ResultComponent } from './result/result.component';
 import { PlaceTipsComponent } from './place-tips/place-tips.component';
 import { UserService } from './user.service';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -32,6 +33,7 @@ import { UserService } from './user.service';
     AppRoutingModule,
     FormsModule,
     HttpClientModule,
+    NoopAnimationsModule,
   ],
   providers: [
     SongListService,
