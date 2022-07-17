ef877a6a90a148b8d6e11bb094f948fb6962d1b4
Matthias Klebe <matthiasklebe@gmail.com>
2022-07-17 23:28:59 +0200

    hide legacy lists

--- src/app/categories.ts
+++ src/app/categories.ts
@@ -53,46 +53,46 @@ export const categories: Array<Category> = [
     isClosed: false,
     imageUrl: getImageUrl('202207/21_08_90er_16_9.png.png'),
   },
-  {
-    displayName: 'We Are Family - Die 100 besten Familienlieder',
-    name: 'Top100Family',
-    isClosed: true,
-    imageUrl: 'https://www.radioeins.de/content/dam/rbb/rad/bilder0/202106/musiksommer_familie_16_92.png.png/quality=205/size=300x170.png'
-  },
-  {
-    displayName: 'Magic Numbers - Die 100 besten Songs mit Zahlen im Titel',
-    name: 'Top100Numbers',
-    isClosed: true,
-    imageUrl: 'https://www.radioeins.de/content/dam/rbb/rad/bilder0/202106/musiksommer_zahlen_16_92.png.png/quality=205/size=300x170.png'
-  },
-  {
-    displayName: 'Animals - Die 100 besten Lieder mit Tieren',
-    name: 'Top100Animals',
-    isClosed: true,
-    imageUrl: 'https://www.radioeins.de/content/dam/rbb/rad/bilder0/202106/musiksommer_tiere_16_92.png.png/quality=205/size=300x170.png'
-  },
-  {
-    displayName: 'Flying High - Die 100 besten Drogenlieder',
-    name: 'Top100Drugs',
-    isClosed: true,
-    imageUrl: 'https://www.radioeins.de/content/dam/rbb/rad/bilder0/202106/musiksommer_drogen_16_91.png.png/quality=205/rendition=drogen43n.png/size=300x170.png'
-  },
-  {
-    displayName: 'Eighties - Die 100 besten Lieder der 80er',
-    name: 'Top100Eighties',
-    isClosed: true,
-    imageUrl: 'https://www.radioeins.de/content/dam/rbb/rad/bilder0/202106/top100_80er_16.png.png/quality=205/rendition=musiksommer_achtziger_4_3.png/size=300x170.png'
-  },
-  {
-    displayName: 'On The Road - Die 100 besten Mobilitätslieder',
-    name: 'Top100Mobility',
-    isClosed: true,
-    imageUrl: 'https://www.radioeins.de/content/dam/rbb/rad/bilder0/202106/musiksommer_mobilitaet_16_92.png.png/quality=205/rendition=musiksommer_mobilitaet_4_3.png/size=300x170.png'
-  },
-  {
-    displayName: 'Instrumentals - Die 100 besten Instrumentallieder',
-    name: 'Top100Instrumentals',
-    isClosed: true,
-    imageUrl: 'https://www.radioeins.de/content/dam/rbb/rad/bilder0/202106/musiksommer_instrumental_16_92.png.png/quality=205/rendition=musiksommer_instrumental_4_3.png/size=300x170.png'
-  },
+  // {
+  //   displayName: 'We Are Family - Die 100 besten Familienlieder',
+  //   name: 'Top100Family',
+  //   isClosed: true,
+  //   imageUrl: 'https://www.radioeins.de/content/dam/rbb/rad/bilder0/202106/musiksommer_familie_16_92.png.png/quality=205/size=300x170.png'
+  // },
+  // {
+  //   displayName: 'Magic Numbers - Die 100 besten Songs mit Zahlen im Titel',
+  //   name: 'Top100Numbers',
+  //   isClosed: true,
+  //   imageUrl: 'https://www.radioeins.de/content/dam/rbb/rad/bilder0/202106/musiksommer_zahlen_16_92.png.png/quality=205/size=300x170.png'
+  // },
+  // {
+  //   displayName: 'Animals - Die 100 besten Lieder mit Tieren',
+  //   name: 'Top100Animals',
+  //   isClosed: true,
+  //   imageUrl: 'https://www.radioeins.de/content/dam/rbb/rad/bilder0/202106/musiksommer_tiere_16_92.png.png/quality=205/size=300x170.png'
+  // },
+  // {
+  //   displayName: 'Flying High - Die 100 besten Drogenlieder',
+  //   name: 'Top100Drugs',
+  //   isClosed: true,
+  //   imageUrl: 'https://www.radioeins.de/content/dam/rbb/rad/bilder0/202106/musiksommer_drogen_16_91.png.png/quality=205/rendition=drogen43n.png/size=300x170.png'
+  // },
+  // {
+  //   displayName: 'Eighties - Die 100 besten Lieder der 80er',
+  //   name: 'Top100Eighties',
+  //   isClosed: true,
+  //   imageUrl: 'https://www.radioeins.de/content/dam/rbb/rad/bilder0/202106/top100_80er_16.png.png/quality=205/rendition=musiksommer_achtziger_4_3.png/size=300x170.png'
+  // },
+  // {
+  //   displayName: 'On The Road - Die 100 besten Mobilitätslieder',
+  //   name: 'Top100Mobility',
+  //   isClosed: true,
+  //   imageUrl: 'https://www.radioeins.de/content/dam/rbb/rad/bilder0/202106/musiksommer_mobilitaet_16_92.png.png/quality=205/rendition=musiksommer_mobilitaet_4_3.png/size=300x170.png'
+  // },
+  // {
+  //   displayName: 'Instrumentals - Die 100 besten Instrumentallieder',
+  //   name: 'Top100Instrumentals',
+  //   isClosed: true,
+  //   imageUrl: 'https://www.radioeins.de/content/dam/rbb/rad/bilder0/202106/musiksommer_instrumental_16_92.png.png/quality=205/rendition=musiksommer_instrumental_4_3.png/size=300x170.png'
+  // },
 ]
